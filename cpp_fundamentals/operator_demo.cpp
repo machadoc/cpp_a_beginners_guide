d4ba7431cfc782e29f16cf5e5176a34f5db5240f
machadoc <machadoc@umich.edu>
2025-08-05 21:18:04 -0700

    "demo to show how operators work in C++"

---
+++ cpp_fundamentals/operator_demo.cpp
@@ -0,0 +1,20 @@
+// using an operatot
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int length;  // this declares a variable
+  int with;    // this declares another variable
+  int area;    // this does too
+
+  length = 7;  // this assigns 7 to length
+  with = 5;    // this assigns 5 to with
+
+  area = length * with;  // compute area
+
+  cout << " the area is ";
+  cout << area;  // this displays 35
+
+  return 0;
+}
