228b65a919460285a2037a244fecc35e7df2e573
machadoc <machadoc@umich.edu>
2025-08-05 21:22:08 -0700

    "simplified operator demo"

---
+++ cpp_fundamentals/simplified_operator_demo.cpp
@@ -0,0 +1,17 @@
+// a simplified version of the are program
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int length;  // this declares a variable
+  int width;   // this declares another variable
+
+  length = 7;  // this assigns 7 to length
+  width = 5;   // this assigns 5 to width
+
+  cout << "The area is ";
+  cout << length * width;  // this displays 35
+
+  return 0;
+}
