73333e35b62c70a63f9527f43e190087a76333b6
machadoc <machadoc@umich.edu>
2025-08-05 21:01:18 -0700

    "file to demonstate variable declaration and initialization in C++."

---
+++ cpp_fundamentals/var_demo.cpp
@@ -0,0 +1,15 @@
+// using a variable
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int length; // this declares a variable
+
+    length = 7; // this assigns 7 to the variable length
+
+    cout << "The length is ";
+    cout << length; // this dispays 7
+
+    return 0;
+}
